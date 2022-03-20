9f42dce656f2ca0527bab1632f986101a1d98fa3
samuelbarrett <72281080+sirsamuelb@users.noreply.github.com>
2022-03-20 14:52:46 -0500

    add authorization handling

--- components/spotify/spotifyApi.ts
+++ components/spotify/spotifyApi.ts
@@ -1,20 +1,66 @@
 // requests to Spotify API through spotify-web-api-node library
 
 let spotifyWebApi = require('spotify-web-api-node');
-import { read } from 'fs';
+import { query } from 'express';
+import { URLSearchParams } from 'url';
 import auth from '../../auth.json';
+import web from '../../web.json';
 
 const scopes = 'user-library-read';
 const stateKey = 'spotify-auth-state';
+let state: string;
 
 const spotify = new spotifyWebApi({
 	clientId: auth.CLIENT_ID,
 	clientSecret: auth.SECRET,
-	redirectUri: auth.REDIRECT_URI
+	redirectUri: web.REDIRECT_SIGNEDIN
 });
 
 // request user authentication from Spotify Accounts Service
 let login = function(req: any, res: any) {
+	state = getRandomString(16);
+
+	res.cookie(stateKey, state);
+
+	res.redirect('https://accounts.spotify.com/authorize?' + new URLSearchParams([
+		["response_type", "code"],
+		["client_id", encodeURIComponent(auth.CLIENT_ID)],
+		["scope", encodeURIComponent(scopes)],
+		["redirect_uri", web.REDIRECT_SIGNEDIN],
+		["state", state]
+	]).toString());
+};
+
+let getAuthorization = function(req: any, res: any) {
+	let returnState = req.query.state || null;
+	let code = req.query.code || null;
+	let error = req.query.error || null;
+
+	if (returnState === state) {
+		if (code !== null) {
+			spotify.authorizationCodeGrant(code).then(
+				function(data: any) {
+				  	console.log('The token expires in ' + data.body['expires_in']);
+				  	console.log('The access token is ' + data.body['access_token']);
+				  	console.log('The refresh token is ' + data.body['refresh_token']);
+			  
+				  	spotify.setAccessToken(data.body['access_token']);
+				  	spotify.setRefreshToken(data.body['refresh_token']);
+					
+					res.redirect(`${web.ORIGIN}${web.PORT}/spotify/${web.AUTHORIZATION_SUCCESS_ENDPOINT}`);
+				},
+				function(err: any) {
+				  console.log('Something went wrong!', err);
+				}
+			)
+		}
+	} else {
+		console.log(error);
+	}
+};
+
+// request from user library
+let getUserSongsData = function(req: any, res: any) {
 	
 };
 
@@ -29,5 +75,5 @@ let getRandomString = function(length: Number) {
 	return randomString;
 }
 
-export { spotify };
+export { spotify, login, getAuthorization };
 
--- components/spotify/spotifyRoute.ts
+++ components/spotify/spotifyRoute.ts
@@ -1,11 +1,21 @@
 // router for the spotify component
 
 import express from 'express';
+import { getAuthorization, login } from './spotifyApi';
+import web from '../../web.json';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.send('okay here is spotify');
+	login(req, res);
+});
+
+router.get('signed-in/', (req, res) => {
+	getAuthorization(req, res);
+});
+
+router.get(`${web.AUTHORIZATION_SUCCESS_ENDPOINT}`, (req, res) => {
+	
 });
 
 export { router as spotifyRouter };
