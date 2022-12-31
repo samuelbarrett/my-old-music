8d0a41fff8d2088cffe24c3221ed46094c802ed9
samuelbarrett <72281080+sirsamuelb@users.noreply.github.com>
2022-12-30 19:18:43 -0600

    add sourcemaps for debugging (still using vs code to debug though)

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,23 @@ import path from 'path';
 // replacement for __dirname constant from CommonJS, missing in ES Modules.
 import * as url from 'url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const dev = process.env.NODE !== 'production';
 
 export default {
 	target: 'node',
 	entry: './src/index.js',
+	devtool: dev ? 'eval-cheap-module-source-map' : false,
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'public/dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				enforce: 'pre',
+				use: ['source-map-loader'],
+			}
+		]
 	}
 };
